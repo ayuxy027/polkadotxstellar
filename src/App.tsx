73a6c30b52bcd29d729f4b1c9e47c57559068a02
ghoshvidip26 <ghoshvidip26@gmail.com>
2025-12-05 20:47:05 +0530

    chore: FetchWalletInfo

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
 import { WalletProvider } from "./wallet/WalletContext";
+import FetchWalletInfo from "./components/FetchWalletInfo";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/about" element={<LandingPage />} />
           <Route path="/login" element={<LandingPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/wallet-info" element={<FetchWalletInfo />} />
         </Routes>
       </BrowserRouter>
     </WalletProvider>
